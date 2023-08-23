aab84ef837d445f9efbab409b9a7df40b422bf60
Rotosama <n.ruizgarcia@hotmail.com>
2023-08-23 16:21:02 +0200

    initial commit without devise

--- db/migrate/20230823130628_create_workers.rb
+++ db/migrate/20230823130628_create_workers.rb
@@ -1,8 +1,6 @@
 class CreateWorkers < ActiveRecord::Migration[7.0]
   def change
     create_table :workers do |t|
-      t.string :email
-      t.string :password
       t.string :name
       t.string :last_name
       t.string :dni
--- db/migrate/20230823130952_create_department.rb
+++ db/migrate/20230823130952_create_department.rb
@@ -1,7 +1,6 @@
 class CreateDepartment < ActiveRecord::Migration[7.0]
   def change
     create_table :departments do |t|
-      t.references :worker, foreign_key: true
       t.string :name
       
       t.timestamps
