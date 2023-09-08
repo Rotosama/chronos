653b39dd9eb710214bf437870ceadbc06ae461d2
Lucas Garcia <lucas@crocantickets.com>
2023-09-08 13:48:00 +0200

    migration errors fixed

--- db/migrate/20230908103408_add_devise_to_workers.rb
+++ db/migrate/20230908103408_add_devise_to_workers.rb
@@ -6,8 +6,6 @@ class AddDeviseToWorkers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.boolean :admin, default: false
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/migrate/20230908104220_add_admin_to_workers.rb
+++ db/migrate/20230908104220_add_admin_to_workers.rb
@@ -1,6 +1,6 @@
 class AddAdminToWorkers < ActiveRecord::Migration[7.0]
   def change
-    add_column :workers, :admin, :boolean
+    add_column :workers, :admin, :boolean, default: false
     remove_column :workers, :role, :integer
   end
 end
