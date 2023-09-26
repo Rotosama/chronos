ac8a97fb3666b9880fa715da2be11290d9c0f58d
Nacho <nacho@entradium.com>
2023-09-26 12:43:03 +0200

    design

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -4,4 +4,8 @@ class TimeEntry < ApplicationRecord
 
   validates :entry_date, presence: true
   validates :exit_date, presence: true, allow_nil: true
+
+  def closed?
+    exit_date.present?
+  end
 end
